7577ddcbdd9edc37802259ee3576649565f625d5
Your Name <you@example.com>
2024-09-13 18:26:22 +0800

    initial commit

---
+++ comparison.js
@@ -0,0 +1,37 @@
+let favoriteNumber1 = 24;
+let favoriteNumber2 = 18;
+
+let greaterThan = favoriteNumber1 > favoriteNumber2;
+console.log('favoriteNumber1 greater than favoriteNumber2 ?' , greaterThan);
+
+let  lessThan = favoriteNumber1 < favoriteNumber2;
+console.log('favoriteNumber1 less than favoriteNumber2 ?' , lessThan);
+
+let greaterThanEqualto = favoriteNumber1 >= favoriteNumber2;
+console.log( 'favoriteNumber1 greater than or equal to favoriteNumber2 ?',greaterThanEqualto);
+
+let lessThanEqualto = favoriteNumber1 <= favoriteNumber2;
+console.log( 'favoriteNumber1 less than or equal to favoriteNumber2 ?' , lessThanEqualto);
+
+let strictEquality = favoriteNumber1 === favoriteNumber1;
+console.log( 'favoriteNumber1 strictequality favoriteNumber2 ?',strictEquality);
+
+let looseEquality = favoriteNumber1 == favoriteNumber2;
+console.log( 'favoriteNumber1 looseEquality favoriteNumber2 ?',looseEquality);
+
+let strictInequality = favoriteNumber1 !== favoriteNumber2;
+console.log('favoriteNumber1 strictEquality favoriteNumber2 ?',strictInequality);
+
+let LooseInequality = favoriteNumber1 != favoriteNumber2;
+console.log( 'favoriteNumber1 LooseInequality favoriteNumber2 ?',LooseInequality);
+
+let and = favoriteNumber1 >0 && favoriteNumber2 >0;
+console.log(and);
+
+let or = favoriteNumber1 >0 || favoriteNumber2 >0;
+console.log(or);
+
+let Not = !(favoriteNumber1 == favoriteNumber2);
+console.log(Not);
+
+// Denver Valentino BSCS 2 A ACTIVITY 4
